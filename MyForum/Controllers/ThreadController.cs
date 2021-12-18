537988fbf1e4afc13e14c7b412ce20bf14fcb971
SkyZen <kirill102b@gmail.com>
2021-12-18 11:18:13 +0400

    Добавленна возможность удаления статей, учитывая роли.

--- MyForum/Controllers/ThreadController.cs
+++ MyForum/Controllers/ThreadController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MyForum.Data.Models;
+using MyForum.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -69,8 +70,33 @@ namespace MyForum.Controllers
                     .FirstOrDefaultAsync(i => i.Id == id);
             var com = Enumerable.Reverse(await context.Comments
                 .Include(u => u.Users)
+                .Include(u => u.Users.Avatar)
+                .Include(r => r.Users.Role)
                 .Include(th => th.Thread).ToListAsync()).ToList();
             return View(Tuple.Create(user,th,com));
+        }
+        [HttpPost]
+        public async Task<IActionResult> DeleteThread(int id)
+        {
+           Threads th = await context.Threads
+                    .Include(u => u.Autor)
+                    .Include(u => u.Autor.Avatar)
+                    .Include(r => r.Autor.Role)
+                    .FirstOrDefaultAsync(i => i.Id == id);
+            context.Threads.Remove(th);
+            await context.SaveChangesAsync();
+            User user = await context.Users
+                      .Include(u => u.Avatar)
+                      .Include(u => u.Role)
+                      .FirstOrDefaultAsync(u => u.Name == User.Identity.Name);
+            ChangePasswordViewModel changePassModel = new ChangePasswordViewModel();
+            var list = Enumerable.Reverse(await context.Users
+                .Include(u => u.Avatar)
+                .Include(u => u.Role).ToListAsync()).Take(5).ToList();
+            var list2 = Enumerable.Reverse(await context.Threads
+                .Include(u => u.Autor)
+                .ToListAsync()).Take(5).ToList();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
